7c0c463c047eeb81b44dd2e19b18e3ef31e559a6
Richard <richytong@gmail.com>
2019-05-09 15:49:41 -0700

    test this

---
+++ arbs.sh
@@ -0,0 +1,182 @@
+#!/bin/sh
+# Auto Rice Boostrapping Script (arbs)
+
+dotfilesrepo="https://github.com/richytong/dotfiles.git"
+progsfile="https://raw.githubusercontent.com/richytong/arbs/master/deps.csv"
+aurhelper="yay"
+
+error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
+
+getuserandpass() { \
+	# Prompts user for new username an password.
+	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+	while ! echo "$name" | grep "^[a-z_][a-z0-9_-]*$" >/dev/null 2>&1; do
+		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done
+	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
+	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	while ! [ "$pass1" = "$pass2" ]; do
+		unset pass2
+		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done ;}
+
+usercheck() { \
+	! (id -u "$name" >/dev/null) 2>&1 ||
+	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. \\Zboverwrite\\Zn ???" 14 70
+	}
+
+preinstallmsg() { \
+	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit; }
+	}
+
+adduserandpass() { \
+	# Adds user `$name` with password $pass1.
+	dialog --infobox "Adding user \"$name\"..." 4 50
+	useradd -m -g wheel -s /bin/bash "$name" >/dev/null 2>&1 ||
+	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+	echo "$name:$pass1" | chpasswd
+	unset pass1 pass2 ;}
+
+refreshkeys() { \
+	dialog --infobox "Refreshing Arch Keyring..." 4 40
+	pacman --noconfirm -Sy archlinux-keyring >/dev/null 2>&1
+	}
+
+newperms() { # Set special sudoers settings for install (or after).
+	sed -i "/#ARBS/d" /etc/sudoers
+	echo "$* #ARBS" >> /etc/sudoers ;}
+
+manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
+	[ -f "/usr/bin/$1" ] || (
+	dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
+	cd /tmp || exit
+	rm -rf /tmp/"$1"*
+	curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
+	sudo -u "$name" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
+	cd "$1" &&
+	sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
+	cd /tmp || return) ;}
+
+maininstall() { # Installs all needed programs from main repo.
+	dialog --title "ARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
+	}
+
+gitmakeinstall() {
+	dir=$(mktemp -d)
+	dialog --title "LARBS Installation" --infobox "Installing \`$(basename "$1")\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
+	git clone --depth 1 "$1" "$dir" >/dev/null 2>&1
+	cd "$dir" || exit
+	make >/dev/null 2>&1
+	make install >/dev/null 2>&1
+	cd /tmp || return ;}
+
+aurinstall() { \
+	dialog --title "ARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+	echo "$aurinstalled" | grep "^$1$" >/dev/null 2>&1 && return
+	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+	}
+
+installationloop() { \
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
+	total=$(wc -l < /tmp/progs.csv)
+  aurinstalled=$(pacman -Qm | awk '{print $1}')
+	while IFS=, read -r tag program comment; do
+		n=$((n+1))
+		echo "$comment" | grep "^\".*\"$" >/dev/null 2>&1 && comment="$(echo "$comment" | sed "s/\(^\"\|\"$\)//g")"
+		case "$tag" in
+			"") maininstall "$program" "$comment" ;;
+      "A") aurinstall "$program" "$comment" ;;
+			"G") gitmakeinstall "$program" "$comment" ;;
+		esac
+	done < /tmp/progs.csv ;}
+
+adddockeruser() { \
+  # Creates docker group and adds user `$name` to docker group
+  dialog --infobox "Adding user \"$name\" to docker..." 4 50
+  groupadd "docker"
+  gpasswd -a "$name" "docker"
+  }
+
+putgitrepo() { # Downlods a gitrepo $1 and places the files in $2 only overwriting conflicts
+	dialog --infobox "Downloading and installing config files..." 4 60
+	dir=$(mktemp -d)
+	[ ! -d "$2" ] && mkdir -p "$2" && chown -R "$name:wheel" "$2"
+	chown -R "$name:wheel" "$dir"
+	sudo -u "$name" git clone --depth 1 "$1" "$dir/gitrepo" >/dev/null 2>&1 &&
+	sudo -u "$name" cp -rfT "$dir/gitrepo" "$2"
+	}
+
+serviceinit() { for service in "$@"; do
+	dialog --infobox "Enabling \"$service\"..." 4 40
+	systemctl enable "$service"
+	systemctl start "$service"
+	done ;}
+
+systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+	rmmod pcspkr
+	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
+
+### THE ACTUAL SCRIPT ###
+
+### This is how everything happens in an intuitive format and order.
+
+# Check if user is root on Arch distro. Install dialog.
+pacman -Syu --noconfirm --needed dialog || error "Are you sure you're running this as the root user? Are you sure you're using an Arch-based distro? ;-) Are you sure you have an internet connection? Are you sure your Arch keyring is updated?"
+
+# Get and verify username and password.
+# getuserandpass || error "User exited."
+
+# Give warning if user already exists.
+# usercheck || error "User exited."
+
+# Last chance for user to back out before install.
+preinstallmsg || error "User exited."
+
+# Add user and pass
+# adduserandpass || error "Error adding username and/or password."
+
+# Refresh Arch keyrings.
+refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
+
+dialog --title "ARBS Installation" --infobox "Installing \`basedevel\` and \`git\` for installing other software." 5 70
+pacman --noconfirm --needed -S base-devel git >/dev/null 2>&1
+[ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
+
+# Allow user to run sudo without password. Since AUR programs must be installed
+# in a fakeroot environment, this is required for all builds with AUR.
+# newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
+
+# Make pacman and yay colorful and adds eye candy on the progress bar because why not.
+grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color/Color/" /etc/pacman.conf
+grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+
+# Use all cores for compilation.
+sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+
+# The command that does all the installing. Reads the progs.csv file and
+# installs each needed program the way required. Be sure to run this only after
+# the user has been created and has priviledges to run sudo without a password
+# and all build dependencies are installed.
+installationloop
+
+# Add docker user
+adddockeruser || error "Error adding user to docker group"
+
+# Install the dotfiles in the user's home directory
+# putgitrepo "$dotfilesrepo" "/home/$name"
+# rm -f "/home/$name/README.md" "/home/$name/LICENSE"
+
+# Enable services here.
+serviceinit NetworkManager cronie docker
+
+# Point docker image storage to home
+
+
+# Most important command! Get rid of the beep!
+systembeepoff
+
+# All done
+dialog --title "All done!" --msgbox "Hey congrats" 12 80
+clear
